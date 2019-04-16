539dda66cb353ea85bab4f6702fe2ec59c377f12
Harsh Pandya <pandyaharsh43@gmail.com>
2019-04-16 17:49:18 -0500

    Updates

---
+++ precache-manifest.85b29c784cdcaba225d1a61ef0ab70be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f413a2fc5801996c6bb",
+    "url": "/Season-app/./static/js/runtime~main.1ef7f376.js"
+  },
+  {
+    "revision": "7a5f3983f2c9af551912",
+    "url": "/Season-app/./static/js/main.b3c2e474.chunk.js"
+  },
+  {
+    "revision": "ade766e263d11c89b214",
+    "url": "/Season-app/./static/js/2.f147ce16.chunk.js"
+  },
+  {
+    "revision": "7a5f3983f2c9af551912",
+    "url": "/Season-app/./static/css/main.164538ff.chunk.css"
+  },
+  {
+    "revision": "4a2b4d5a19046a595dc16df804eb71a1",
+    "url": "/Season-app/./index.html"
+  }
+];
